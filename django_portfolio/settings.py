4eff9c653c72b14ce89c92e6dfc0d58f3c246661
derianavila <derianpro0728@gmail.com>
2026-01-28 10:03:04 -0500

    arreglos2.0 commit

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -144,17 +144,18 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# =========================
+# STORAGES
+# =========================
 STORAGES = {
-    # ✅ MEDIA / UPLOADS (admin, imágenes, pdf)
     "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
-
-    # ✅ STATIC FILES (WhiteNoise)
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
 
 
 # =====================
@@ -163,15 +164,4 @@ STORAGES = {
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-# =====================
-# DEFAULT
-# =====================
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    },
-}
+
